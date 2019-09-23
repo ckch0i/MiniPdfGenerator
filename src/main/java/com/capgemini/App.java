e0a8ae0e2d4e9e467c4724295c484547f8e04929
ckchoi <ck.choi@capgemini.com>
2019-09-23 15:12:51 +0200

    use of classloader

--- src/main/java/com/capgemini/App.java
+++ src/main/java/com/capgemini/App.java
@@ -6,31 +6,38 @@ import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.pdf.BaseFont;
 import com.itextpdf.text.pdf.PdfWriter;
 import com.itextpdf.xmp.XMPException;
+import lombok.SneakyThrows;
 
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 
 public class App {
 
-    private static final String pathToGetFontsFrom = "./src/main/resources/fonts";
-    private static final String outputFile = "c:/temp/PrintAllFonts.pdf";
+    private static final String PATH_TO_GET_FONTS_FROM = "fonts";
+    private static final String OUTPUT_FILE = "PrintAllFonts.pdf";
 
-    public static void main(String[] args) throws DocumentException, XMPException, IOException {
+    public static void main(String[] args) throws DocumentException, XMPException, IOException, URISyntaxException {
         generateFontsDocument();
+        //        loadFiles();
+    }
+
+    public static void loadFiles() throws URISyntaxException, IOException {
+        Files.newDirectoryStream(Paths.get(App.class.getClassLoader().getResource(PATH_TO_GET_FONTS_FROM).toURI()),
+                "*.{ttf}").forEach(f -> System.out.println(f.getFileName())).;
     }
 
     private static void generateFontsDocument() throws IOException {
         BasePdfDocument basePdfDocument = new BasePdfDocument();
 
         try {
-            PdfWriter writer = PdfWriter.getInstance(basePdfDocument, new FileOutputStream(outputFile));
+            PdfWriter writer = PdfWriter.getInstance(basePdfDocument, new FileOutputStream(OUTPUT_FILE));
             basePdfDocument.open();
-
-            ArrayList<String> fonts = getFonts(pathToGetFontsFrom);
+            ArrayList<String> fonts = getFonts(PATH_TO_GET_FONTS_FROM);
             writeFonts(basePdfDocument, fonts);
             basePdfDocument.close();
             writer.close();
@@ -42,21 +49,20 @@ public class App {
     }
 
     private static void writeFonts(BasePdfDocument basePdfDocument, ArrayList<String> fonts) {
-        fonts.forEach(font -> {
-            try {
-                basePdfDocument.add(new Paragraph("Text written in font: " + font,
-                        FontFactory.getFont(String.valueOf(font), BaseFont.WINANSI, BaseFont.EMBEDDED, 16)));
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        });
+        fonts.forEach(font -> tryCatcher(basePdfDocument, font));
     }
 
-    private static ArrayList<String> getFonts(String pathToGetfilesFrom) throws IOException {
-        ArrayList<String> files = new ArrayList<>();
-        Files.list(Paths.get(pathToGetfilesFrom)).filter(i -> i.toString().endsWith(".ttf"))
-                .forEach(i -> files.add(i.toString()));
-        return files;
+    @SneakyThrows
+    private static void tryCatcher(BasePdfDocument basePdfDocument, String font) {
+        basePdfDocument.add(new Paragraph("Text written in font: " + font,
+                FontFactory.getFont(String.valueOf(font), BaseFont.WINANSI, BaseFont.EMBEDDED, 16)));
     }
+
+        private static ArrayList<String> getFonts(String pathToGetfilesFrom) throws IOException {
+            ArrayList<String> files = new ArrayList<>();
+            Files.list(Paths.get(pathToGetfilesFrom)).filter(i -> i.toString().endsWith(".ttf"))
+                    .forEach(i -> files.add(i.toString()));
+            return files;
+        }
 
 }
