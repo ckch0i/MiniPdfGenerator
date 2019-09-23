5c8c96ba37100f3428e222b387972d8f00fbba41
ckchoi <ck.choi@capgemini.com>
2019-09-23 14:42:20 +0200

    refactored

--- src/main/java/com/capgemini/App.java
+++ src/main/java/com/capgemini/App.java
@@ -13,28 +13,25 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.function.Consumer;
 
 public class App {
 
+    private static final String pathToGetFontsFrom = "./src/main/resources/fonts";
+    private static final String outputFile = "c:/temp/PrintAllFonts.pdf";
+
     public static void main(String[] args) throws DocumentException, XMPException, IOException {
+        generateFontsDocument();
+    }
+
+    private static void generateFontsDocument() throws IOException {
         BasePdfDocument basePdfDocument = new BasePdfDocument();
-        String pathToGetFilesFrom = "./src/main/resources/fonts";
 
         try {
-            PdfWriter writer = PdfWriter
-                    .getInstance(basePdfDocument, new FileOutputStream("c:/temp/PrintAllFonts.pdf"));
+            PdfWriter writer = PdfWriter.getInstance(basePdfDocument, new FileOutputStream(outputFile));
             basePdfDocument.open();
 
-            ArrayList<String> fonts = getFonts(pathToGetFilesFrom);
-            fonts.forEach(font -> {
-                try {
-                    basePdfDocument.add(new Paragraph("Testing of font " + font,
-                            FontFactory.getFont(String.valueOf(font), BaseFont.WINANSI, BaseFont.EMBEDDED, 16)));
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            });
+            ArrayList<String> fonts = getFonts(pathToGetFontsFrom);
+            writeFonts(basePdfDocument, fonts);
             basePdfDocument.close();
             writer.close();
         } catch (DocumentException e) {
@@ -44,21 +41,22 @@ public class App {
         }
     }
 
+    private static void writeFonts(BasePdfDocument basePdfDocument, ArrayList<String> fonts) {
+        fonts.forEach(font -> {
+            try {
+                basePdfDocument.add(new Paragraph("Text written in font: " + font,
+                        FontFactory.getFont(String.valueOf(font), BaseFont.WINANSI, BaseFont.EMBEDDED, 16)));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
     private static ArrayList<String> getFonts(String pathToGetfilesFrom) throws IOException {
         ArrayList<String> files = new ArrayList<>();
         Files.list(Paths.get(pathToGetfilesFrom)).filter(i -> i.toString().endsWith(".ttf"))
                 .forEach(i -> files.add(i.toString()));
         return files;
-    }
-
-    static Consumer<BasePdfDocument> lambdaWrapper(Consumer<BasePdfDocument> consumer) throws Exception {
-        return i -> {
-            try {
-                consumer.accept(i);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        };
     }
 
 }
